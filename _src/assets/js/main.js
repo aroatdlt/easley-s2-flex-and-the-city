a2826c953e702d339fc77d062623b0fea1d90221
laurasanra <laura.san.rada@gmail.com>
2018-11-29 20:27:52 +0100

    preview name and job

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-console.log('>> Ready :)');
+const userNameForm = document.querySelector('.full__name');
+const professionForm = document.querySelector('.profession__input');
+
+const userNameCard = document.querySelector('.name');
+const professionCard = document.querySelector('.profession');
+
+const fillCard = () => {
+    userNameCard.innerHTML = userNameForm.value;
+    professionCard.innerHTML = professionForm.value;
+}
+
+userNameForm.addEventListener('keyup', fillCard);
+professionForm.addEventListener('keyup', fillCard);
