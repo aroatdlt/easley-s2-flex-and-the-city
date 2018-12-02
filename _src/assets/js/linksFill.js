6c4ef9796099b7203bf85c63cb67b9864479cd35
joanajimfer <joanajimfer@gmail.com>
2018-12-02 18:44:16 +0100

    Provisional changes

---
+++ _src/assets/js/linksFill.js
@@ -0,0 +1,12 @@
+const inputGithub = document.querySelector('.input_box__github');
+
+const linkToGithub = document.querySelector('.github__link');
+
+const fillLinkGithub = () => {
+    if(inputGithub.value === "") {
+        linkToGithub.href = "";
+    }else {
+        inputGithub.valur = 
+        linkToGithub.href
+    }
+}
