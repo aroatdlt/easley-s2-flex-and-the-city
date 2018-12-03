a1a13924a1150cc16b32552cac5310c01bcf6586
joanajimfer <joanajimfer@gmail.com>
2018-12-03 20:51:02 +0100

    Links to the card

--- _src/assets/js/previewlinksinicons.js
+++ _src/assets/js/previewlinksinicons.js
@@ -1,29 +1,40 @@
-const mobileForm = document.querySelector('.email__input');
-const emailForm = document.querySelector('.phone__input');
+const mobileForm = document.querySelector('.phone__input');
+const emailForm = document.querySelector('.email__input');
 const linkedinForm = document.querySelector('.linkedin__input');
 const gitHubForm = document.querySelector('.github__input');
 
-const mobileLink = document.querySelector('.icon__number').href;
-const emailLink = document.querySelector('.icon__mail').href;
-const linkedinLink = document.querySelector('.icon__linkedin').href;
-const githubLink = document.querySelector('.icon__github').href;
+const mobileLink = document.querySelector('.icon__number');
+const emailLink = document.querySelector('.icon__mail');
+const linkedinLink = document.querySelector('.icon__linkedin');
+const githubLink = document.querySelector('.icon__github');
 
-const fillMobileNumber = () => {
-    if(mobileForm === ""){
-        mobileLink.href = "";
-    }else {
-        mobileLink.href = `tel:`+ mobileForm;
-    }
+function fillLinkedin(event) {
+    const linkedinText = linkedinForm.value;
+    linkedinLink.href = 'https://www.' + linkedinText;
+};
+
+linkedinForm.addEventListener('keyup', fillLinkedin);
+
+function fillMail(event) {
+    const mailText = emailForm.value;
+    emailLink.href = 'mailto:' + mailText;
+};
+
+emailForm.addEventListener('keyup', fillMail);
+
+function fillMobileNumber(event) {
+    const telText = mobileForm.value;
+    mobileLink.href = `tel:` + telText;
 };
 
 mobileForm.addEventListener('keyup', fillMobileNumber);
 
-const fillGithubLink = () => {
-    if(gitHubForm.value === "") {
-        githubLink = "";
-    }else {
-        githubLink = gitHubForm.value;
-    }
+/* Enlace del link con la página de github
+*/
+
+function fillGithubLink(event) {
+    const gitText = gitHubForm.value;
+    githubLink.href = 'https://github.com/' + gitText;
 };
 
 gitHubForm.addEventListener('keyup', fillGithubLink);
