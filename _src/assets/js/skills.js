110f376414b902972801121abe424c279113aafb
aroatdlt <aroatato@gmx.com>
2018-12-04 20:57:43 +0100

    Add new code for draw skills in card creator

--- _src/assets/js/skills.js
+++ _src/assets/js/skills.js
@@ -1,7 +1,34 @@
 const skillsInputElements = document.querySelectorAll ('.checkbox__input');
-console.log(skillsInputElements);
+const skillsLabelElements = document.querySelectorAll ('.checkbox__label');
 
 const skillsListElement = document.querySelector ('.skill__tags');
+
+const skills = [];
+
+
+function handleCheckBoxClick (event) {
+    const checkBoxClick = event.currentTarget;
+    for(let i=0; i < skillsInputElements.length; i++) {
+        if (skillsInputElements[i].checked === true){
+            skills.push(skillsInputElements[i].value);
+            skillsListElement.innerHTML = `<li class="tag">${skills[i]}</li>`;
+        }
+    }  
+}
+
+
+for(let i=0; i < skillsInputElements.length; i++) {
+    skillsInputElements[i].addEventListener('click', handleCheckBoxClick);
+
+}
+
+
+
+
+
+
+
+
 
 
 
