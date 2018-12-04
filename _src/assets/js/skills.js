9ed4a03aae3135ef4e483cabed0817daf0d7bce0
aroatdlt <aroatato@gmx.com>
2018-12-04 20:11:15 +0100

    Change querySelectorAll

--- _src/assets/js/skills.js
+++ _src/assets/js/skills.js
@@ -1,7 +1,9 @@
-const skillHTMLElement = document.querySelector('.checkbox__input--html');
-const skillCSSElement = document.querySelector('.checkbox__input--css');
-const skillReactElement = document.querySelector('.checkbox__input--react');
-const skillJavascriptElement = document.querySelector('.checkbox__input--javascript');
-const skillGitElement = document.querySelector('.checkbox__input--git');
-const skillGulpElement = document.querySelector('.checkbox__input--gulp');
-const skillSassElement = document.querySelector('.checkbox__input--sass');
+const skillsInputElements = document.querySelectorAll ('.checkbox__input');
+console.log(skillsInputElements);
+
+const skillsListElement = document.querySelector ('.skill__tags');
+
+
+
+
+
