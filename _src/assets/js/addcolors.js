884da9904ffa8964b170aa1afe59caa801664c11
joanajimfer <joanajimfer@gmail.com>
2018-12-08 14:36:55 +0100

    All palet colors added

--- _src/assets/js/addcolors.js
+++ _src/assets/js/addcolors.js
@@ -8,7 +8,25 @@ const buttonsColor = document.querySelectorAll('.icon__item');
 const circleColor = document.querySelectorAll('.social__media-buttom');
 const tagColors = document.querySelectorAll('.tag');
 
+/* Adding the original palette
+*/
+function addFirstPalete (event) {
+    nameColor.classList.add('add__color__dark-green');
+    rectangleColor.classList.add('add__color__light-green');
+    for (let i=0;i<tagColors.length;i++) {
+        tagColors[i].classList.add('add__color__medium-green');
+    }
+    for (let i=0;i<buttonsColor.length;i++) {
+        buttonsColor[i].classList.add('add__color__dark-green');
+    }
+    for (let i=0;i<circleColor.length;i++) {
+        circleColor[i].classList.add('add__border__light-green');
+    }
+};
 
+
+/* Adding red palette color 
+*/
 function addSecondPalete (event) {
     nameColor.classList.add('add__color__dark-red');
     rectangleColor.classList.add('add__color__light-red');
@@ -21,8 +39,26 @@ function addSecondPalete (event) {
     for (let i=0;i<circleColor.length;i++) {
         circleColor[i].classList.add('add__border__light-red');
     }
-
 };
 
+/* Adding grey palette color 
+*/
+function addThirdPalete (event) {
+    nameColor.classList.add('add__color__dark-blue');
+    rectangleColor.classList.add('add__color__light-blue');
+    for (let i=0;i<tagColors.length;i++) {
+        tagColors[i].classList.add('add__color__medium-blue');
+    }
+    for (let i=0;i<buttonsColor.length;i++) {
+        buttonsColor[i].classList.add('.add__color__dark-blue');
+    }
+    for (let i=0;i<circleColor.length;i++) {
+        circleColor[i].classList.add('add__border__light-blue');
+    }
+};
 
+/* Listeners from color card function 
+*/
 secondPalete.addEventListener('click', addSecondPalete);
+thirdPalete.addEventListener('click', addThirdPalete);
+firstPalete.addEventListener('click', addFirstPalete);
