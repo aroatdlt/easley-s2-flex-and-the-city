0ae49ea1d0777e0d2d01bc406c6c44644916f579
laurasanra <laura.san.rada@gmail.com>
2018-12-10 22:28:41 +0100

    align labels and inputs

--- _src/assets/js/petitionofSkills.js
+++ _src/assets/js/petitionofSkills.js
@@ -1,19 +1,18 @@
 
-const formFillCollapsable = document.querySelector('.collapseform-fill');
+/*const formFillCollapsable = document.querySelector('.collapseform-fill');*/
 const skillContainer = document.querySelector('.container_skills');
 
+function showSkills() {
 
-
-function showSkills(){
-    
     fetch('https://raw.githubusercontent.com/Adalab/dorcas-s2-proyecto-data/master/skills.json')
-    .then(response => response.json())
-    .then(data => {
-        const totalSkills = data.skills;
-        for (const skill of totalSkills) {
-            skillContainer.innerHTML += `           <input class="checkbox__input checkbox__input--${skill}" id="${skill}" type="checkbox" name="skill_option" value="${skill}"><label class="checkbox__label checkbox__label--${skill}" for="${skill}">${skill}</label>`;
-    }
-    });
+        .then(response => response.json())
+        .then(data => {
+            const totalSkills = data.skills;
+            console.log(totalSkills);
+            for (const skill of totalSkills) {
+                skillContainer.innerHTML += `<input class="checkbox__input checkbox__input--${skill}" id="${skill}" type="checkbox" name="skill_option" value="${skill}"><label class="checkbox__label checkbox__label--${skill}" for="${skill}">${skill}</label>`;
+            }
+        });
 
 };
 showSkills();
