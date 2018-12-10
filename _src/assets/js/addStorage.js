b8c367f5097aef98dcec360d6d63fd476245a384
joanajimfer <joanajimfer@gmail.com>
2018-12-10 20:39:08 +0100

    local storage name and profession

--- _src/assets/js/addStorage.js
+++ _src/assets/js/addStorage.js
@@ -1,7 +1,7 @@
 let userInfo = {
     "palette": 1,
     "typography": 2,
-    "name" : "María García",
+    "name" : "",
     "job": "Front-end developer",
     "phone": "+34 666666666",
     "email": "mariagar@example.com",
@@ -11,4 +11,10 @@ let userInfo = {
     "skills": ["HTML", "Sass", "JavaScript"]
   }
 
-  
+
+  /* First we storage our object in local storage
+  */
+
+
+
+ 
