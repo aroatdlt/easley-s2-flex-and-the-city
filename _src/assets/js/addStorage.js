b1cb46cb4293e792bdcf56e124080c9a9cc9dcb9
joanajimfer <joanajimfer@gmail.com>
2018-12-10 20:08:36 +0100

    initial commit

---
+++ _src/assets/js/addStorage.js
@@ -0,0 +1,14 @@
+let userInfo = {
+    "palette": 1,
+    "typography": 2,
+    "name" : "María García",
+    "job": "Front-end developer",
+    "phone": "+34 666666666",
+    "email": "mariagar@example.com",
+    "linkedin": "mariagar",
+    "github": "mariagar",
+    "photo": "data:image/png;base64,2342ba...",
+    "skills": ["HTML", "Sass", "JavaScript"]
+  }
+
+  
