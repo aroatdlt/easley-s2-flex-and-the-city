c7caeef06384ec241c782e100e546a20b689c89b
laurasanra <laura.san.rada@gmail.com>
2018-12-10 19:41:04 +0100

    xx

--- _src/assets/js/skills.js
+++ _src/assets/js/skills.js
@@ -2,26 +2,56 @@ const skillsInputElements = document.querySelectorAll('.checkbox__input');
 const skillsLabelElements = document.querySelectorAll('.checkbox__label');
 let skillsListElement = document.querySelector('.skill__tags');
 
-
-function handleCheckBoxClick(event){
+/*function handleCheckBoxClick(event){
     
     fetch('https://raw.githubusercontent.com/Adalab/dorcas-s2-proyecto-data/master/skills.json')
     .then(response => response.json())
     .then(data => {
         const skills = data.skills;
-    
-       for (const skillsList of skills) {
+       
+          for (const skillsList of skills) {
             skillsListElement.innerHTML += `<li class="tag">` + skillsList + `</li>`;
-            console.log(skillsList)
-    }
         
+    }
     });
 
 };
 
 for (let i = 0; i < skillsInputElements.length; i++) {
     skillsInputElements[i].addEventListener('click', handleCheckBoxClick);
-};
+};*/
+
+function limitCheckBox() {
+    let count = 0;
+    for (let i = 0; i < skillsInputElements.length; i++) {
+        if (skillsInputElements[i].checked) {
+            count += 1;
+        }
+        if (count > 3 && skillsInputElements[i].checked === false) {
+            skillsInputElements[i].disabled = true;
+        }
+    }
+}
+
+function handleCheckBoxClick(event) {
+
+    let listContent = "";
+
+    for (const skillsItem of skillsInputElements) {
+        if (skillsItem.checked === true) {
+            listContent += `<li class="tag">${skillsItem.value}</li>`;
+        }
+
+    }
+    limitCheckBox();
+    skillsListElement.innerHTML = listContent;
+
+}
+
+for (let i = 0; i < skillsInputElements.length; i++) {
+    skillsInputElements[i].addEventListener('click', handleCheckBoxClick);
+}
+
 
 
 /*function handleCheckBoxClick(event) {
@@ -41,20 +71,8 @@ for (let i = 0; i < skillsInputElements.length; i++) {
 
 for (let i = 0; i < skillsInputElements.length; i++) {
     skillsInputElements[i].addEventListener('click', handleCheckBoxClick);
-}
-
-function limitCheckBox() {
-    let count = 0;
-    for (let i = 0; i < skillsInputElements.length; i++) {
-        if (skillsInputElements[i].checked) {
-            count += 1;
-        }
-        if (count > 3 && skillsInputElements[i].checked === false) {
-            skillsInputElements[i].disabled = true;
-        }
-    }
-
 }*/
+
 
 
 
