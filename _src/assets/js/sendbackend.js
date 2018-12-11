d38a3b4239b3eaf575fe427a96eb246e37f6a821
inmasalcedo <inma.salcedo.gonzalez@gmail.com>
2018-12-11 20:23:08 +0100

    add function send backend

---
+++ _src/assets/js/sendbackend.js
@@ -0,0 +1,20 @@
+function sendRequest(json){
+    fetch('https://us-central1-awesome-cards-cf6f0.cloudfunctions.net/card/', {
+      method: 'POST',
+      body: JSON.stringify(json),
+      headers: {
+        'content-type': 'application/json'
+      },
+    })
+      .then(function(resp) { return resp.json(); })
+      .then(function(result) { showURL(result); })
+      .catch(function(error) { console.log(error); });
+  }
+
+function showURL(result){
+    if(result.success){
+      responseURL.innerHTML = '<a href=' + result.cardURL + '>' + result.cardURL + '</a>';
+    }else{
+      responseURL.innerHTML = 'ERROR:' + result.error;
+    }
+  }
