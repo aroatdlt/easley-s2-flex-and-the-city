4d872696f906c17b21baed6f0002469b9aa410e5
inmasalcedo <inma.salcedo.gonzalez@gmail.com>
2018-12-12 15:46:24 +0100

    created twitter link

--- _src/assets/js/sendbackend.js
+++ _src/assets/js/sendbackend.js
@@ -1,5 +1,6 @@
 const buttonCreateCardElement = document.querySelector('.button_deco');
 const twitterLinkElement = document.querySelector('.twitter_link');
+const twitterAnchor = document.querySelector('.button_twitter');
 function sendRequest(){
     fetch('https://us-central1-awesome-cards-cf6f0.cloudfunctions.net/card/', {
       method: 'POST',
@@ -18,6 +19,7 @@ function sendRequest(){
   function showURL(result){
     if(result.success){
       twitterLinkElement.innerHTML = '<a href=' + result.cardURL + '>' + result.cardURL + '</a>';
+      twitterAnchor.href = `https://twitter.com/intent/tweet?text=${result.cardURL}`;
     }else{
       twitterLinkElement.innerHTML = 'ERROR:' + result.error;
       
