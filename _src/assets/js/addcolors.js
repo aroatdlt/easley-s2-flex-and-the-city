a1017c0c906ea29747a4f05df88d8df74cb3b189
aroatdlt <aroatato@gmx.com>
2018-12-19 15:37:09 +0100

    Indent and deleted coments in addcolor file

--- _src/assets/js/addcolors.js
+++ _src/assets/js/addcolors.js
@@ -1,23 +1,12 @@
 const firstPalete = document.querySelector('#setcolors_green');
 const secondPalete = document.querySelector('#setcolors_red');
 const thirdPalete = document.querySelector('#setcolors_blue');
-
 const nameColor = document.querySelector('.name');
 const rectangleColor = document.querySelector('.rectangle__decoration');
 const buttonsColor = document.querySelectorAll('.icon__item');
 const circleColor = document.querySelectorAll('.social__media-buttom');
 const tagColors = document.querySelectorAll('.tag');
 
-/* Adding the original palette
-
-
-/*function handleLinkedin() {
-    fillLinkedin();
-    userInfo.linkedin = linkedinLink.href;
-    localStorage.setItem('userInfo', JSON.stringify(userInfo));
-
-}
-*/
 function handleFirstPalete() {
     addFirstPalete();
     userInfo.palette = 1;
@@ -34,86 +23,78 @@ function handleThirdPalete() {
     localStorage.setItem('userInfo', JSON.stringify(userInfo));
 }
 
-
-function addFirstPalete () {
+function addFirstPalete() {
     nameColor.classList.add('add__color__dark-green');
     nameColor.classList.remove('add__color__dark-red');
     nameColor.classList.remove('add__color__dark-blue')
     rectangleColor.classList.add('add__color__light-green');
     rectangleColor.classList.remove('add__color__light-blue');
     rectangleColor.classList.remove('add__color__light-red');
-    for (let i=0;i<tagColors.length;i++) {
+    for (let i = 0; i < tagColors.length; i++) {
         tagColors[i].classList.add('add__color__medium-green');
         tagColors[i].classList.remove('add__color__medium-blue');
         tagColors[i].classList.remove('add__color__medium-red');
     }
-    for (let i=0;i<buttonsColor.length;i++) {
+    for (let i = 0; i < buttonsColor.length; i++) {
         buttonsColor[i].classList.add('add__color__dark-green');
         buttonsColor[i].classList.remove('.add__color__dark-blue');
         buttonsColor[i].classList.remove('add__color__dark-red');
     }
-    for (let i=0;i<circleColor.length;i++) {
+    for (let i = 0; i < circleColor.length; i++) {
         circleColor[i].classList.add('add__border__light-green');
         circleColor[i].classList.remove('add__border__light-blue');
         circleColor[i].classList.remove('add__border__light-red');
     }
 };
 
-
-/* Adding red palette color 
-*/
-function addSecondPalete () {
+function addSecondPalete() {
     nameColor.classList.add('add__color__dark-red');
     nameColor.classList.remove('add__color__dark-green');
     nameColor.classList.remove('add__color__dark-blue');
     rectangleColor.classList.add('add__color__light-red');
     rectangleColor.classList.remove('add__color__light-blue');
     rectangleColor.classList.remove('add__color__light-green');
-    for (let i=0;i<tagColors.length;i++) {
+    for (let i = 0; i < tagColors.length; i++) {
         tagColors[i].classList.add('add__color__medium-red');
         tagColors[i].classList.remove('add__color__medium-blue');
         tagColors[i].classList.remove('add__color__medium-green');
     }
-    for (let i=0;i<buttonsColor.length;i++) {
+    for (let i = 0; i < buttonsColor.length; i++) {
         buttonsColor[i].classList.add('add__color__dark-red');
         buttonsColor[i].classList.remove('.add__color__dark-blue');
         buttonsColor[i].classList.remove('add__color__dark-green');
     }
-    for (let i=0;i<circleColor.length;i++) {
+    for (let i = 0; i < circleColor.length; i++) {
         circleColor[i].classList.add('add__border__light-red');
         circleColor[i].classList.remove('add__border__light-green');
         circleColor[i].classList.remove('add__border__light-blue');
     }
 };
 
-/* Adding grey palette color 
-*/
-function addThirdPalete () {
+function addThirdPalete() {
     nameColor.classList.add('add__color__dark-blue');
     nameColor.classList.remove('add__color__dark-red');
     nameColor.classList.remove('add__color__dark-green');
     rectangleColor.classList.add('add__color__light-blue');
     rectangleColor.classList.remove('add__color__light-green');
     rectangleColor.classList.remove('add__color__light-red');
-    for (let i=0;i<tagColors.length;i++) {
+    for (let i = 0; i < tagColors.length; i++) {
         tagColors[i].classList.add('add__color__medium-blue');
         tagColors[i].classList.remove('add__color__medium-green');
         tagColors[i].classList.remove('add__color__medium-red');
     }
-    for (let i=0;i<buttonsColor.length;i++) {
+    for (let i = 0; i < buttonsColor.length; i++) {
         buttonsColor[i].classList.add('.add__color__dark-blue');
         buttonsColor[i].classList.remove('add__color__dark-green');
         buttonsColor[i].classList.remove('add__color__dark-red');
     }
-    for (let i=0;i<circleColor.length;i++) {
+    for (let i = 0; i < circleColor.length; i++) {
         circleColor[i].classList.add('add__border__light-blue');
         circleColor[i].classList.remove('add__border__light-green');
         circleColor[i].classList.remove('add__border__light-red');
     }
 };
 
-/* Listeners from color card function 
-*/
 secondPalete.addEventListener('click', handleSecondPalete);
 thirdPalete.addEventListener('click', handleThirdPalete);
 firstPalete.addEventListener('click', handleFirstPalete);
