f536f20cb7d4ad39c7c2818479f43528b3479571
aroatdlt <aroatato@gmx.com>
2018-12-19 15:49:59 +0100

    Delete coment and indent code in previewname file

--- _src/assets/js/previewnamejob.js
+++ _src/assets/js/previewnamejob.js
@@ -1,45 +1,43 @@
 const userNameForm = document.querySelector('.full__name');
 const professionForm = document.querySelector('.profession__input');
-
 const userNameCard = document.querySelector('.name');
 const professionCard = document.querySelector('.profession');
 
-
 function handleCardName() {
-    updateUserInfo('name', userNameForm.value);
-    setLocalStorage();
-    fillCardName();
+  updateUserInfo('name', userNameForm.value);
+  setLocalStorage();
+  fillCardName();
 }
 
 function updateUserInfo(key, value) {
-    userInfo[key] = value;
+  userInfo[key] = value;
 }
 
 function setLocalStorage() {
-    localStorage.setItem('userInfo', JSON.stringify(userInfo));
+  localStorage.setItem('userInfo', JSON.stringify(userInfo));
 }
 
 function fillCardName() {
-    if (userInfo.name !== "") {
-        userNameCard.innerHTML = userInfo.name;
-    } else {
-        userNameCard.innerHTML = "Nombre Apellido";
-    }
+  if (userInfo.name !== "") {
+    userNameCard.innerHTML = userInfo.name;
+  } else {
+    userNameCard.innerHTML = "Nombre Apellido";
+  }
 }
 
 function handleCardProfession() {
-    updateUserInfo('job', professionForm.value);
-    setLocalStorage();
-    fillCardProfession();
+  updateUserInfo('job', professionForm.value);
+  setLocalStorage();
+  fillCardProfession();
 }
 
 function fillCardProfession() {
-    if(userInfo.job !== "") {
-        professionCard.innerHTML = userInfo.job;
-        console.log(userInfo.job);
-    }else {
-        professionCard.innerHTML = "Front-end developer";
-    }
+  if (userInfo.job !== "") {
+    professionCard.innerHTML = userInfo.job;
+    console.log(userInfo.job);
+  } else {
+    professionCard.innerHTML = "Front-end developer";
+  }
 }
 
 userNameForm.addEventListener('keyup', handleCardName);
