b7d5acf03073b2d71013414d6834f891341c2d7a
aroatdlt <aroatato@gmx.com>
2018-12-19 15:45:07 +0100

    Indent and delete coments in addtypography file

--- _src/assets/js/addtypography.js
+++ _src/assets/js/addtypography.js
@@ -1,29 +1,26 @@
 const ubuntuTypo = document.querySelector('#Ubuntu');
 const comicSansTypo = document.querySelector('#ComicSans');
 const montserratTypo = document.querySelector('#Montserrat');
-
 const nameTypo = document.querySelector('.name');
 const professionTypo = document.querySelector('.profession');
-
 
 function selectUbuntu() {
   addUbuntuTypo();
   userInfo.typography = 1;
   localStorage.setItem('userInfo', JSON.stringify(userInfo));
-}
+};
 
 function selectComicSans() {
   addComicSansTypo();
   userInfo.typography = 2;
   localStorage.setItem('userInfo', JSON.stringify(userInfo));
-}
+};
 
 function selectMontserrat() {
   addMonserratTypo();
   userInfo.typography = 3;
   localStorage.setItem('userInfo', JSON.stringify(userInfo));
-}
-
+};
 
 function addUbuntuTypo() {
   nameTypo.classList.add('add__font__ubuntu');
