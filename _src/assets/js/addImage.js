28a1842d0316bdd7c0a2b0a6b19ba61ce0e75ba0
aroatdlt <aroatato@gmx.com>
2018-12-19 15:41:21 +0100

    Indent and deleted comments in addImage file

--- _src/assets/js/addImage.js
+++ _src/assets/js/addImage.js
@@ -1,30 +1,25 @@
 const fr = new FileReader();
-
 const uploadBtn = document.querySelector('.button_add-image');
-
 const fileField = document.querySelector('#img-selector');
 const profileImage = document.querySelector('.photo__user');
 const littleImage = document.querySelector('.image-preview');
 
-function getImage(e){
+function getImage(e) {
   var myFile = e.currentTarget.files[0];
   fr.addEventListener('load', writeImage);
   fr.readAsDataURL(myFile);
 }
 
 function writeImage() {
-  profileImage.src= fr.result;
-  littleImage.src= fr.result;
+  profileImage.src = fr.result;
+  littleImage.src = fr.result;
   userInfo.photo = fr.result;
   setLocalStorage();
 }
 
 function fakeFileClick() {
- fileField.click(); 
+  fileField.click();
 }
-
 
 fileField.addEventListener('change', getImage);
 uploadBtn.addEventListener('click', fakeFileClick);
-
-
