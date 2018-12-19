9afb82d197bd24be80670cd45c4fdfa06623e84f
aroatdlt <aroatato@gmx.com>
2018-12-19 15:53:20 +0100

    Deleted coment code and indent search file

--- _src/assets/js/searchfromlocalstorage.js
+++ _src/assets/js/searchfromlocalstorage.js
@@ -1,69 +1,57 @@
 let savedUserInfo = JSON.parse(localStorage.getItem('userInfo'));
 
-function refillFormUserInfo(){
-    if (savedUserInfo !== null){
-        userInfo = savedUserInfo;
-        refillFormField(userNameForm, 'name');
-        refillFormField(professionForm, 'job');
-        refillFormField(linkedinForm, 'linkedin');
-        refillFormField(emailForm, 'email');
-        refillFormField(mobileForm, 'phone');
-        refillFormField(gitHubForm, 'github');
+function refillFormUserInfo() {
+  if (savedUserInfo !== null) {
+    userInfo = savedUserInfo;
+    refillFormField(userNameForm, 'name');
+    refillFormField(professionForm, 'job');
+    refillFormField(linkedinForm, 'linkedin');
+    refillFormField(emailForm, 'email');
+    refillFormField(mobileForm, 'phone');
+    refillFormField(gitHubForm, 'github');
 
-        fillCardName();
-        fillCardProfession();
-        fillLinkedin();
-        fillMail();
-        fillMobileNumber();
-        fillGithubLink();
+    fillCardName();
+    fillCardProfession();
+    fillLinkedin();
+    fillMail();
+    fillMobileNumber();
+    fillGithubLink();
 
-        /*Typographi refill */
-        if(userInfo.typography === 1) {
-            addUbuntuTypo();
-            ubuntuTypo.setAttribute('checked', 'true');
-        } else if(userInfo.typography === 2) {
-            addComicSansTypo();
-            comicSansTypo.setAttribute('checked', 'true');
-        } else if(userInfo.typography === 3) {
-            addMonserratTypo();
-            montserratTypo.setAttribute('checked', 'true');
-        }
-        
-        /*Color palettes refill */
-        if(userInfo.palette === 1) {
-            addFirstPalete();
-            firstPalete.setAttribute('checked', 'true');
-        } else if (userInfo.palette === 2) {
-            addSecondPalete();
-            secondPalete.setAttribute('checked', 'true');
-        } else if (userInfo.palette === 3) {
-            addThirdPalete();
-            thirdPalete.setAttribute('checked', 'true');
-        }
+    if (userInfo.typography === 1) {
+      addUbuntuTypo();
+      ubuntuTypo.setAttribute('checked', 'true');
+    } else if (userInfo.typography === 2) {
+      addComicSansTypo();
+      comicSansTypo.setAttribute('checked', 'true');
+    } else if (userInfo.typography === 3) {
+      addMonserratTypo();
+      montserratTypo.setAttribute('checked', 'true');
+    }
 
-        /*Skills refill*/
-        const skillsInObject = userInfo.skills;
-        let listContentSkillsLS = "";
-        let skillsListElement = document.querySelector('.skill__tags');
-        for(let i = 0; i<skillsInObject.length; i++) {
-            listContentSkillsLS += `<li class="tag">${skillsInObject[i]}</li>`;
-        } 
-        skillsListElement.innerHTML = listContentSkillsLS;
-        const skillChildren = skillContainer.querySelectorAll('.checkbox__input');
-        /* console.log(skillChildren);
-        for(let i = 0; i<skillChildren.length; i++) {
-            //Si array de local storage contiene elemento actual
-        if(skillsInObject.includes(skillChildren[i].value)) {
-            skillChildren[i].setAttribute('checked', 'true');
-        }  
-}*/
-     } 
-    
-  
+    if (userInfo.palette === 1) {
+      addFirstPalete();
+      firstPalete.setAttribute('checked', 'true');
+    } else if (userInfo.palette === 2) {
+      addSecondPalete();
+      secondPalete.setAttribute('checked', 'true');
+    } else if (userInfo.palette === 3) {
+      addThirdPalete();
+      thirdPalete.setAttribute('checked', 'true');
+    }
+
+    const skillsInObject = userInfo.skills;
+    let listContentSkillsLS = "";
+    let skillsListElement = document.querySelector('.skill__tags');
+    for (let i = 0; i < skillsInObject.length; i++) {
+      listContentSkillsLS += `<li class="tag">${skillsInObject[i]}</li>`;
+    }
+    skillsListElement.innerHTML = listContentSkillsLS;
+    const skillChildren = skillContainer.querySelectorAll('.checkbox__input');
+  }
 }
 
-function refillFormField(inputField, key) {
-    inputField.value = userInfo[key];
+function refillFormField(inputField, key){
+  inputField.value = userInfo[key];
 }
 
 refillFormUserInfo();
