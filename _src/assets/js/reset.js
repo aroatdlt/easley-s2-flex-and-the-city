8a8eb79156129ca2e712e8914cbb9f7f31bdd903
lauraferrandof <lauraferrandof@gmail.com>
2018-12-20 16:43:51 +0100

    Fix reset function

--- _src/assets/js/reset.js
+++ _src/assets/js/reset.js
@@ -4,8 +4,16 @@ const fillForm = document.querySelector('.fill-form');
 const shareForm = document.querySelector('.share-form');
 
 function handleResetBtn () {
+    skillsListElement = document.querySelector('.skill__tags');
     professionCard.innerHTML = "Front-end developer";
     userNameCard.innerHTML = "Nombre Apellido";
+    linkedinLink.href = '';
+    emailLink.href = '';
+    mobileLink.href = '';
+    githubLink.href = '';
+    addComicSansTypo();
+    addFirstPalete();
+    skillsListElement.innerHTML = '<li class="first_tag tag">HTML</li> <li class="second_tag tag">CSS</li> <li class="third_tag tag">GULP</li>';
     localStorage.removeItem('userInfo');
     designForm.reset();
     fillForm.reset();
