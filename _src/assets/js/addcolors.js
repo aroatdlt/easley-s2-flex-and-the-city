61ceeb976518d2372c9e0a50d9a9a1766cf7602e
rdfornieles <rdfornieles@gmail.com>
2019-01-08 19:59:04 +0100

    finish refactoring

--- _src/assets/js/addcolors.js
+++ _src/assets/js/addcolors.js
@@ -1,11 +1,9 @@
 const firstPalete = document.querySelector('#setcolors_green');
 const secondPalete = document.querySelector('#setcolors_red');
 const thirdPalete = document.querySelector('#setcolors_blue');
-//const nameColor = document.querySelector('.name');
 const rectangleColor = document.querySelector('.rectangle__decoration');
 const buttonsColor = document.querySelectorAll('.icon__item');
 const circleColor = document.querySelectorAll('.social__media-buttom');
-//const tagColors = document.querySelectorAll('.tag');
 
 function handleFirstPalete() {
     addFirstPalete();
@@ -27,83 +25,60 @@ function handleThirdPalete() {
 
 function addFirstPalete() {
     const tagColors = document.querySelectorAll('.tag');
-    //nameColor.classList.add('add__color__dark-green');
-    //nameColor.classList.remove('add__color__dark-red');
-    //nameColor.classList.remove('add__color__dark-blue')
     rectangleColor.classList.add('add__color__light-green');
     rectangleColor.classList.remove('add__color__light-blue', 'add__color__light-red');
-    //rectangleColor.classList.remove('add__color__light-red');
   
-
-
     for (let i = 0; i < tagColors.length; i++) {
         tagColors[i].classList.add('add__color__medium-green');
         tagColors[i].classList.remove('add__color__medium-blue', 'add__color__medium-red');
-        //tagColors[i].classList.remove('add__color__medium-red');
     }
    
-    
     for (let i = 0; i < buttonsColor.length; i++) {
         buttonsColor[i].classList.add('add__color__dark-green');
         buttonsColor[i].classList.remove('.add__color__dark-blue', 'add__color__dark-red');
-        //buttonsColor[i].classList.remove('add__color__dark-red');
     }
+
     for (let i = 0; i < circleColor.length; i++) {
         circleColor[i].classList.add('add__border__light-green');
         circleColor[i].classList.remove('add__border__light-blue', 'add__border__light-red');
-        //circleColor[i].classList.remove('add__border__light-red');
     }
 };
 
 function addSecondPalete() {
     const tagColors = document.querySelectorAll('.tag');
-    //nameColor.classList.add('add__color__dark-red');
-    //nameColor.classList.remove('add__color__dark-green');
-    //nameColor.classList.remove('add__color__dark-blue');
     rectangleColor.classList.add('add__color__light-red');
     rectangleColor.classList.remove('add__color__light-blue', 'add__color__light-green');
-    //rectangleColor.classList.remove('add__color__light-green');
 
     for (let i = 0; i < tagColors.length; i++) {
         tagColors[i].classList.add('add__color__medium-red');
         tagColors[i].classList.remove('add__color__medium-blue', 'add__color__medium-green');
-        //tagColors[i].classList.remove('add__color__medium-green');
     }
     for (let i = 0; i < buttonsColor.length; i++) {
         buttonsColor[i].classList.add('add__color__dark-red');
         buttonsColor[i].classList.remove('.add__color__dark-blue', 'add__color__dark-green');
-        //buttonsColor[i].classList.remove('add__color__dark-green');
     }
     for (let i = 0; i < circleColor.length; i++) {
         circleColor[i].classList.add('add__border__light-red');
         circleColor[i].classList.remove('add__border__light-green', 'add__border__light-blue');
-        //circleColor[i].classList.remove('add__border__light-blue');
     }
 };
 
 function addThirdPalete() {
     const tagColors = document.querySelectorAll('.tag');
-    //nameColor.classList.add('add__color__dark-blue');
-    //nameColor.classList.remove('add__color__dark-red');
-    //nameColor.classList.remove('add__color__dark-green');
     rectangleColor.classList.add('add__color__light-blue');
     rectangleColor.classList.remove('add__color__light-green', 'add__color__light-red');
-    //rectangleColor.classList.remove('add__color__light-red');
 
     for (let i = 0; i < tagColors.length; i++) {
         tagColors[i].classList.add('add__color__medium-blue');
         tagColors[i].classList.remove('add__color__medium-green', 'add__color__medium-red');
-        //tagColors[i].classList.remove('add__color__medium-red');
     }
     for (let i = 0; i < buttonsColor.length; i++) {
         buttonsColor[i].classList.add('.add__color__dark-blue');
         buttonsColor[i].classList.remove('add__color__dark-green', 'add__color__dark-red');
-        //buttonsColor[i].classList.remove('add__color__dark-red');
     }
     for (let i = 0; i < circleColor.length; i++) {
         circleColor[i].classList.add('add__border__light-blue');
         circleColor[i].classList.remove('add__border__light-green', 'add__border__light-red');
-        //circleColor[i].classList.remove('add__border__light-red');
     }
 };
 
